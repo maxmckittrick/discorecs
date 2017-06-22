18c5d9cfce601c5575a69525350263528d5165a3
Max McKittrick <max.m.mckittrick@gmail.com>
2017-06-22 11:58:19 -0400

    Add files via upload

---
+++ examples/hadoop/hadoop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2015 Insight Data Science
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PEG_ROOT=$(dirname ${BASH_SOURCE})/../..
+
+CLUSTER_NAME=hadoop-cluster
+
+peg up ${PEG_ROOT}/examples/hadoop/namenode.yml &
+peg up ${PEG_ROOT}/examples/hadoop/datanodes.yml &
+
+wait
+
+peg fetch ${CLUSTER_NAME}
+
+peg install ${CLUSTER_NAME} ssh
+peg install ${CLUSTER_NAME} hadoop
