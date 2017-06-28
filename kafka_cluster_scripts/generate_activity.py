bcd969f5ae45f694d29845fb96331f1a6a5fa513
Max McKittrick <max.m.mckittrick@gmail.com>
2017-06-28 12:08:26 -0400

    Delete generate_activity.py

--- kafka_cluster_scripts/generate_activity.py
+++
@@ -1,3 +0,0 @@
-#generate pseudo-random activity for a given discogs user from cassandra and post to kafka topic (not working)
-
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic user-collection-stream
